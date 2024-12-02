7e6a1038602d7ed09baa5e4fa686faf35918d2b8
idrak28 <idrakefaz99@gmail.com>
2024-12-02 19:14:02 +0600

    message_board

--- chaiurDjango/chai/urls.py
+++ chaiurDjango/chai/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.all_chai, name='all_chai') ,
     path('<int:chai_id>/',views.chai_detials, name='chai_details') ,
+    path('chai_stores/',views.chai_store_view, name='chai_stores') ,
+
+
 ]
--- chaiurDjango/chai/views.py
+++ chaiurDjango/chai/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import ChaiVarityForm
 from django.shortcuts import get_object_or_404
 # Create your views here.
-from .models import ChaiVarity
+from .models import ChaiVarity , Store
 def all_chai(request ) :
     chais = ChaiVarity.objects.all()
     return render(request ,'chai/all_chai.html', {'chais': chais})
@@ -11,3 +11,18 @@ def all_chai(request ) :
 def chai_detials (request , chai_id) :
     chai = get_object_or_404(ChaiVarity , pk=chai_id)
     return render (request , 'chai/chai_details.html' , {'chai':chai})
+
+
+
+def chai_store_view(request):
+    stores =None 
+    if request.method =='POST' :
+        form = ChaiVarityForm(request.POST)
+
+        if form.is_valid() :
+            chai_variety = form.cleaned_data['chai_varity_form']
+            stores = Store.objects.filter(chai_varities=chai_variety)
+            print(stores)
+    else :
+        form = ChaiVarityForm()
+    return render (request,'chai/chai_stores.html', {'stores ': stores , 'form' : form})
