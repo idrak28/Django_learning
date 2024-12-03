cde4dc53d9cf1cdd4010a12d49896715575a184e
idrak28 <idrakefaz99@gmail.com>
2024-12-03 14:38:17 +0600

    user_login_added

--- blogs/blog/urls.py
+++ blogs/blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path 
 from .views import BlogListView , BlogDetailView ,BlogCreateView ,BlogUpdateView ,BlogDeleteView
+
+
+
+
+
 urlpatterns = [
 path ("post/<int:pk>/edit/",BlogUpdateView.as_view(),name="post_edit"),
 path("post/<int:pk>/",BlogDetailView.as_view(),name="post_detail"),
--- blogs/django_project/settings.py
+++ blogs/django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog" ,
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
